f05ee1f6fa7f71d60822a3a27982fea5cc435016
Muhammad Fahad <mughalfahad544@gmail.com>
2025-01-08 15:55:07 +0500

    Update AppDelegate.swift

--- uber_drivers_app/ios/Runner/AppDelegate.swift
+++ uber_drivers_app/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyClGLbuireUmfElTaZ2UQWV7bejJKSbXDE")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
