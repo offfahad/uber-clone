90e43d5005aa2bc1523d8e57368d9558b5cf3613
Muhammad Fahad <mughalfahad544@gmail.com>
2025-01-08 15:51:27 +0500

    Update global_var.dart

--- uber_users_app/lib/global/global_var.dart
+++ uber_users_app/lib/global/global_var.dart
@@ -5,9 +5,9 @@ String userName = "";
 String userPhone = "";
 String userEmail = "";
 String userID = FirebaseAuth.instance.currentUser!.uid;
-const String googleMapKey = "AIzaSyClGLbuireUmfElTaZ2UQWV7bejJKSbXDE";
-const String stripeSecretAPIKey = "sk_test_51PQ234P0mKjkBKKZw6dAxgSI4YQjxf9QVIKrZXyLe05y9fAmrxlcOc2RO5Za7yQhI4Utby88PedaaVFv25hrgxys008Pj28E57";
-const String stripePublishedKey = "pk_test_51PQ234P0mKjkBKKZdaEzl1I52zd0GB6pxjaIwYfrryNHtx3FO9aYTTEi4HZKWfyj50mBdgzh3YHQRRkC8nVo9V4Z00eS2JtTsn";
+const String googleMapKey = "";
+const String stripeSecretAPIKey = "";
+const String stripePublishedKey = "";
 const CameraPosition googlePlexInitialPosition = CameraPosition(
   target: LatLng(37.42796133580664, -122.085749655962),
   zoom: 14.4746,
